3c36d1de79d71914a2b7ed4425dd9fe6874d2964
Mahidhar Narra <narram@mail.nih.gov>
2013-06-25 10:55:22 -0400

    merge the ETL branch and restructure folders

---
+++ ETL/bin/notify.sh
@@ -0,0 +1,13 @@
+#setup your home directory
+HOME_DIR=/home/local/HCIT/oawofolu
+
+## First Kettle home needs to be set so that you get the variables set.
+export KETTLE_HOME=${HOME_DIR}
+
+## We need to run the next shell script in /home/oawofolu because it will look for simple-jndi folder under where the script is run from
+cd ${HOME_DIR}/cure_etl
+
+##This command runs the full ETL to populate DWH Database
+${HOME_DIR}/PDI-4.3/kitchen.sh -file=${HOME_DIR}/cure_etl/kettle/Notify/Notify.kjb -param:PROPERTIES=${HOME_DIR}/cure_etl/conf/PROCESS_XML.properties -level=Detailed -log=${HOME_DIR}/cure_etl/logs/notify.log
+
+cd ${HOME_DIR}
